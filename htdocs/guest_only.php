87f20280e61b0627b67271bc317cb7de63c1fd6e
junghum <superjunghum@naver.com>
2016-08-14 17:46:46 +0900

    no message

--- htdocs/guest_only.php
+++ htdocs/guest_only.php
@@ -3,7 +3,7 @@ if ( isLogined() === true ) {
 	if ( isAjax() ) {
 		$resultData = [
 			'resultCode' => 'F-1',
-			'msg' => '이미 로그인 되어 있습니다.'
+			'msg' => '이미 로그인 되어 있습니다....'
 		];
 
 		echo json_encode($resultData);
